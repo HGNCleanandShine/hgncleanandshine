4bb16f90ebc426f6f99774a27abd2543026bb266
HGNCleanandShine <dinroutray@gmail.com>
2024-06-25 23:49:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
-var searchPic;
+var images;
 function LoadImages() {
-    searchPic = new Image(100,100);
-    searchPic.src = "XXXX/YYYY/search.png";
-    // This is correct and the path is correct
+    logo = new Image(100,100);
+    logo.src = "logo.png";
 }
