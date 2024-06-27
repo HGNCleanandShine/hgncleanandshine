b91b38b77bbf7f66da0556cfc79a4f22d2f90077
HGNCleanandShine <dinroutray@gmail.com>
2024-06-26 22:04:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-var images;
-function LoadImages() {
-    logo = new Image(100,100);
-    logo.src = "https://github.com/HGNCleanandShine/hgncleanandshine/assets/173873341/a38b0c2d-151f-4c54-9fe2-e8e6c07";
-}
+
